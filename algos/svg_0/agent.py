Fix batching bug in svg0, adjust KL priors to properly regulate

algos/svg_0/agent.py
@@ -16,14 +16,16 @@ class SVG0(nn.Module):
         obs_dim,
         action_dim,
         action_lim,
+        device,
         lr=3e-4,
         tau=5e-3,
         gamma=0.95,
-        batch_size=5,
+        batch_size=256,
         update_epochs=5,
         update_interval=2,
         activation=nn.ReLU,
         hidden_sizes=[64, 64],
+        use_target_entropy=False,
         **kwargs,
     ):
         super().__init__()
@@ -32,14 +34,15 @@ class SVG0(nn.Module):
         self.update_step = 0
         self.tau = tau
         self.gamma = gamma
+        self.batch_size = batch_size
         self.action_limit = action_lim
         self.update_epochs = update_epochs
         self.update_interval = update_interval
-        self.batch_size = batch_size
+        self.use_target_entropy = use_target_entropy
 
         # Policy network
         self.pi = StochasticPolicy(
-            obs_dim, action_dim, action_lim, hidden_sizes, activation
+            obs_dim, action_dim, hidden_sizes, activation
         )
 
         # Q functions networks
@@ -61,6 +64,12 @@ class SVG0(nn.Module):
             self.q_optim, 1, 1e-6, total_iters=3000
         )
 
+        # Temperature for target entropy to compare with KL priors
+        if self.use_target_entropy:
+            self.log_alpha = torch.zeros(1, requires_grad=True).to(device)
+            self.temp_optimizer = optim.Adam([self.log_alpha], lr=lr)
+    
+
     def update_target(self):
         # Update target networks by polyak averaging.
         with torch.no_grad():
@@ -74,6 +83,8 @@ class SVG0(nn.Module):
         if deterministic:
             action = mean
 
+
+        action.clamp_(-self.action_limit, self.action_limit)
         return action
 
     def optimize(self, batch, global_step):
@@ -87,10 +98,10 @@ class SVG0(nn.Module):
             )
 
             batch_indices = next(iter(batch_indices))
-            batch = {k: v[batch_indices] for k, v in batch.items()}
+            mini_batch = {k: v[batch_indices] for k, v in batch.items()}
 
             # Update Q functions
-            q_loss, q_1, q_2 = self._compute_q_loss(batch)
+            q_loss, q_1, q_2 = self._compute_q_loss(mini_batch)
 
             self.q_optim.zero_grad()
             q_loss.backward()
@@ -102,10 +113,15 @@ class SVG0(nn.Module):
                     p.requires_grad = False
 
                 # Update stochastic policy
-                pi_loss, pi = self._compute_policy_loss(batch)
+                pi_loss, pi, temp_loss = self._compute_policy_loss(mini_batch)
                 self.pi_optim.zero_grad()
                 pi_loss.backward()
                 self.pi_optim.step()
+
+                if self.use_target_entropy:
+                    self.temp_optimizer.zero_grad()
+                    temp_loss.backward()
+                    self.temp_optimizer.step()
 
                 for p in self.q.parameters():
                     p.requires_grad = True
@@ -169,12 +185,16 @@ class SVG0(nn.Module):
     def _compute_policy_loss(self, batch):
         b_obs = batch["obs"]
 
-        b_action, _, _ = self.pi(b_obs, with_logp=False)
+        b_action, b_logprobs, _ = self.pi(b_obs, with_logp=True)
         q_b1, q_b2 = self.q(b_obs, b_action)
         b_q_values = torch.min(q_b1, q_b2)
 
-        # TODO: Learn the temperature for entropy regularization
-        # self.alpha.detach() * logp_a
+        if self.use_target_entropy:
+            alpha = self.log_alpha.exp()
+            policy_loss = (alpha * b_logprobs - b_q_values).mean()
+            temp_loss = -(alpha * b_logprobs.detach()).mean()
+        else:
+            policy_loss = (-b_q_values).mean()
+            temp_loss = None
 
-        policy_loss = (-b_q_values).mean()
-        return policy_loss, b_action
+        return policy_loss, b_action, temp_loss

algos/svg_0_kl_prior/agent.py
@@ -19,12 +19,12 @@ class SVG0(nn.Module):
         lr=3e-4,
         tau=5e-3,
         gamma=0.95,
-        batch_size=5,
+        batch_size=256,
         update_epochs=5,
         update_interval=2,
         activation=nn.ReLU,
         hidden_sizes=[64, 64],
-        prior_obs_dim=1,
+        prior_obs_dim=2,
         prior_coeff=0.01,
         **kwargs,
     ):
@@ -32,23 +32,23 @@ class SVG0(nn.Module):
 
         # Optimize variables
         self.update_step = 0
-        self.gamma = gamma
         self.tau = tau
+        self.gamma = gamma
+        self.batch_size = batch_size
         self.prior_coeff = prior_coeff
-        self.max_grad_norm = 0.5  # TODO: Does norm help?
         self.action_limit = action_lim
+        self.prior_obs_dim = prior_obs_dim
         self.update_epochs = update_epochs
         self.update_interval = update_interval
-        self.batch_size = batch_size
 
         # Policy network
         self.pi = StochasticPolicy(
-            obs_dim, action_dim, action_lim, hidden_sizes, activation
+            obs_dim, action_dim, hidden_sizes, activation
         )
 
         # Prior policy network
         self.prior_pi = StochasticPolicy(
-            prior_obs_dim, action_dim, action_lim, hidden_sizes, activation
+            prior_obs_dim, action_dim, hidden_sizes, activation
         )
 
         # Q functions networks
@@ -59,11 +59,12 @@ class SVG0(nn.Module):
         for p in self.target_q.parameters():
             p.requires_grad = False
 
-        # Set up optimizers for policy and value function
+        # Set up optimizers for policy, prior and value function
         self.q_optim = torch.optim.Adam(self.q.parameters(), lr=lr)
         self.pi_optim = torch.optim.Adam(self.pi.parameters(), lr=lr)
         self.prior_optim = torch.optim.Adam(self.prior_pi.parameters(), lr=lr)
 
+        # TODO: Prior scheduler?
         self.pi_scheduler = torch.optim.lr_scheduler.LinearLR(
             self.pi_optim, 1, 1e-6, total_iters=3000
         )
@@ -79,11 +80,12 @@ class SVG0(nn.Module):
 
     def act(self, obs, deterministic=False):
         with torch.no_grad():
-            action, _, mean = self.pi(obs)
+            action, _, mean, _ = self.pi(obs)
 
         if deterministic:
             action = mean
 
+        action.clamp_(-self.action_limit, self.action_limit)
         return action
 
     def optimize(self, batch, global_step):
@@ -97,12 +99,13 @@ class SVG0(nn.Module):
             )
 
             batch_indices = next(iter(batch_indices))
-            batch = {k: v[batch_indices] for k, v in batch.items()}
+            mini_batch = {k: v[batch_indices] for k, v in batch.items()}
 
             # Compute KL divergence between pi and prior pi
+            # TODO: Taking assymetric prior samples needs to be done properly
             with torch.no_grad():
-                _, _, _, pi = self.pi(batch["obs"])
-            _, _, _, prior_pi = self.prior_pi(batch["obs"][:, :1])
+                _, _, _, pi = self.pi(mini_batch["next_obs"])
+            _, _, _, prior_pi = self.prior_pi(mini_batch["next_obs"][:, :self.prior_obs_dim])
             kl_reg = kl_divergence(pi, prior_pi)
             loss_kl = kl_reg.sum()
 
@@ -110,12 +113,11 @@ class SVG0(nn.Module):
             self.prior_optim.zero_grad()
             loss_kl.backward()
             self.prior_optim.step()
-
+            
             # Update Q functions
-            q_loss, q_1, q_2 = self._compute_q_loss(batch, kl_reg)
+            q_loss, q_1, q_2 = self._compute_q_loss(mini_batch)
 
             self.q_optim.zero_grad()
-            nn.utils.clip_grad_norm_(self.q.parameters(), self.max_grad_norm)
             q_loss.backward()
             self.q_optim.step()
 
@@ -125,12 +127,11 @@ class SVG0(nn.Module):
                     p.requires_grad = False
 
                 # Update stochastic policy
-                pi_loss, pi = self._compute_policy_loss(batch)
-                pi_loss = pi_loss - self.prior_coeff * loss_kl.detach()
+                pi_loss, pi = self._compute_policy_loss(mini_batch)
+                pi_loss = pi_loss - (self.prior_coeff * loss_kl.detach())
 
                 self.pi_optim.zero_grad()
                 pi_loss.backward()
-                nn.utils.clip_grad_norm_(self.pi.parameters(), self.max_grad_norm)
                 self.pi_optim.step()
 
                 for p in self.q.parameters():
@@ -149,7 +150,7 @@ class SVG0(nn.Module):
         writer.add_histogram("SVG0/Q1_histogram", q_1, global_step)
         writer.add_histogram("SVG0/Q2_histogram", q_2, global_step)
 
-    def _compute_q_loss(self, batch, kl_reg):
+    def _compute_q_loss(self, batch):
 
         b_obs, b_act, b_rew, b_next_obs, b_done = (
             batch["obs"],
@@ -160,10 +161,14 @@ class SVG0(nn.Module):
         )
 
         with torch.no_grad():
-            b_next_action, next_log_probs, b_next_mean, _ = self.pi(
+            b_next_action, _, b_next_mean, pi = self.pi(
                 b_next_obs, with_logp=False
             )
+            _, _, _, prior_pi = self.prior_pi(b_next_obs[:, :self.prior_obs_dim])
 
+            # \bar{KL}_t' = KL[pi(. | s_t')||prior_pi(. | s_t')]
+            kl_reg = kl_divergence(pi, prior_pi)
+            
             target_noise = b_next_action - b_next_mean
             b_next_action = b_next_mean + target_noise
 
@@ -173,7 +178,7 @@ class SVG0(nn.Module):
             q_target = torch.min(q_t1, q_t2)
 
             # TD target
-            value_target = (b_rew - self.prior_coeff * kl_reg) + (
+            value_target = (b_rew - (self.prior_coeff * kl_reg)) + (
                 1.0 - b_done
             ) * self.gamma * q_target
 

algos/svg_0_kl_prior/core.py
@@ -45,7 +45,7 @@ class StochasticPolicy(nn.Module):
 
         mean = torch.tanh(mu)
 
-        return action, logp_a, mean
+        return action, logp_a, mean, pi_distribution
 
 
 class DoubleQFunction(nn.Module):

main.py
@@ -29,6 +29,7 @@ def main(config, agent_cls):
         obs_dim=env.observation_space.shape[0],
         action_dim=env.action_space.shape[0],
         action_lim=env.action_space.high[0],
+        device=config["device"],
         **config["svg0"],
     ).to(config["device"])
 
@@ -70,13 +71,14 @@ def main(config, agent_cls):
             global_step += 1
 
         # Log final episode statistics
-        Logger.get().writer.add_scalar("env/return", info["episode"]["r"], global_step)
-        Logger.get().writer.add_scalar("env/length", info["episode"]["l"], global_step)
+        writer = Logger.get().writer
+        writer.add_scalar("env/ep_return", info["episode"]["r"], global_step)
+        writer.add_scalar("env/ep_length", info["episode"]["l"], global_step)
 
         # Store the weights, make a gif, eval and logging
         if episode % config["log_every_n"] == 0 and episode != 0:
 
-            if episode % (config["log_every_n"] * 20) == 0:
+            if episode % (config["log_every_n"] * 5) == 0:
                 make_gif(agent, test_env, episode, config)
 
             # TODO: When implementation is stable
@@ -91,6 +93,9 @@ def main(config, agent_cls):
                 f"test - episode return, length: ({np.mean(test_return):.3f}, "
                 f"{np.mean(test_ep_len):.0f})"
             )
+
+            writer.add_scalar("env/test_ep_return", test_return, global_step)
+            writer.add_scalar("env/test_ep_length", test_ep_len, global_step)
 
 
 def evaluate_policy(agent, env, episodes=10):